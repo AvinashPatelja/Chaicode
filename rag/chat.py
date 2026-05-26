915051de814e3893eea078a35b2e34c61fcfafb6
Avinash Patel <avipatel.ja@gmail.com>
2026-05-26 09:30:20 +0530

    Langchain - Indexing,Retrival

---
+++ rag/chat.py
@@ -0,0 +1,52 @@
+from dotenv import load_dotenv
+from pathlib import Path
+from openai import OpenAI
+from langchain_qdrant import QdrantVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv(Path(__file__).parent / '.env')  # Load environment variables from .env file
+
+client = OpenAI()
+
+# Vector Embeddings
+embeding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+
+# Vector DB connection
+vector_db = QdrantVectorStore.from_existing_collection(
+    collection_name="dotnet-interview-questions",
+    embedding=embeding_model,
+    url="http://localhost:6333",
+)
+
+# Take user query as input
+user_query = input("Enter your query: ")
+
+# Relevant search(chuncks) in vector DB
+search_result = vector_db.similarity_search(query=user_query, k=5)
+
+# context ="\n\n\n".join([f"Page Content: {result.page_content}\nPage Number: {result.metadata['page_label']}\nFile Location: {result.metadata['source']}" for result in search_result])
+
+context = "\n\n".join([
+    result.page_content
+    for result in search_result
+])
+
+print("========== RETRIEVED CONTEXT ==========")
+print(context)
+
+SYSTEM_PROMPT ="""
+You are an expert assistant in resolving user queries using existing context retrieved from pdf files along with page_content, page_number.
+You should only answer the question based on the given context. 
+
+Context: {context}
+"""
+
+response = client.chat.completions.create(
+    model='gpt-4o-mini',
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT}, 
+        {"role": "user", "content": user_query},
+    ]
+)
+
+print(response.choices[0].message.content)
--- rag/index.py
+++ rag/index.py
@@ -1,8 +1,36 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
 
+load_dotenv(Path(__file__).parent / '.env')  # Load environment variables from .env file
+
+# -> pip install -qU langchain-community
+# -> pip install -qU langchain-text-splitter
 pdf_path = Path(__file__).parent / 'DotNetInterviewQuestions.pdf'
 loader = PyPDFLoader(file_path=pdf_path)
+
+# STEP1 -> Load Documents
 docs = loader.load()
 
-print(docs[4])
+# STEP2 -> Split Documents into smaller chunks
+# -> pip install -qU langchain-text-splitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = text_splitter.split_documents(docs)
+
+# STEP3 -> Vector Embeddings
+# ->pip install -qU langchain-openai
+embeding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+# STEP4 -> Create Vector Store and Indexing
+# -> pip install -qU langchain-qdrant
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embeding_model,
+    collection_name="dotnet-interview-questions",
+    url="http://localhost:6333",
+)
+
+print("Indexing completed successfully!")
